7b778046cc0c84f53f3b4181ec8c620e75762d01
Mohammad Al-Samarah <mohammad.alsamarah@hotmail.com>
2015-10-31 16:54:50 +0300

    Create SessionManager.java

---
+++ SessionManager.java
@@ -0,0 +1,92 @@
+package com.twasel;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class SessionManager {
+    // Shared Preferences
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    Editor editor;
+
+    // Context
+    Context _context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Sharedpref file name
+    private static final String PREF_NAME = "SDDappPref";
+
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
+
+    // User name (make variable public to access from outside)
+    public static final String KEY_ID = "id";
+
+    // Email address (make variable public to access from outside)
+
+
+    // Constructor
+    public SessionManager(Context context){
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    /**
+     * Create login session
+     * */
+    public void createLoginSession(String id){
+        // Storing login value as TRUE
+        editor.putBoolean(IS_LOGIN, true);
+
+        // Storing name in pref
+        editor.putString(KEY_ID, id);
+
+        // Storing email in pref
+       // editor.putString(KEY_EMAIL, email);
+
+        // commit changes
+        editor.commit();
+    }
+
+
+    /**
+     * Get stored session data
+     * */
+    public String getUserLoginID(){
+        return  pref.getString(KEY_ID, null);
+    }
+
+    /**
+     * Clear session details
+     * */
+    public void logoutUser(){
+        // Clearing all data from Shared Preferences
+        editor.clear();
+        editor.commit();
+
+        // After logout redirect user to Loing Activity
+        Intent i = new Intent(_context, LoginActivity.class);
+        // Closing all the Activities
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Add new Flag to start new Activity
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Staring Login Activity
+        _context.startActivity(i);
+    }
+
+    /**
+     * Quick check for login
+     * **/
+    // Get Login State
+    public boolean isLoggedIn(){
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+}
